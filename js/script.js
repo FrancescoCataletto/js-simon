2251e34d9105a5ae00ae3da31407c4bd42d49637
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-26 14:26:17 +0200

    better version of random number, with range and added comments

--- js/script.js
+++ js/script.js
@@ -7,15 +7,24 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
  */
 
+// range id numeri determinato da utente
+const numRange = 100;
+// numero di numeri casuali generati
 const numLimit = 5;
+// variabile del numero random
 let randomNum;
 
+// ciclo per generare il num random e stamparlo in html
 for(let i = 0; i < numLimit; i++){
-    randomNum = getRandomNum();
+    randomNum = getRandomNum(1, numRange);
     document.getElementById("random-num").append(randomNum + " ");
 }
 
-function getRandomNum(){
-    return Math.floor(Math.random() * 5 + 1);
+
+
+
+// funzione numero random
+function getRandomNum(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
